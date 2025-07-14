1039bdbe9908ea6ca47140d47374c4156caee813
HavokMoobii <jasonmcounter@gmail.com>
2025-07-13 19:50:55 -0500

    Added get_file_content

---
+++ functions/get_file_content.py
@@ -0,0 +1,23 @@
+import os
+
+from functions.config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    abs_working_directory = os.path.abspath(working_directory)
+    result = ""
+    target = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not target.startswith(abs_working_directory):
+        result = f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        return result
+    if not os.path.isfile(target):
+        result = f'Error: File not found or is not a regular file: "{file_path}"'
+        return result
+    try:
+        with open(target, "r") as f:
+          result = f.read(MAX_CHARS)
+        if os.path.getsize(target) > MAX_CHARS:
+            result += f' [...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        return result
+    except Exception as e:
+        return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -1,22 +1,18 @@
 import unittest
 
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-class TestGetFilesInfo(unittest.TestCase):
-    def test_calculator_to_calculator(self):
-        result = get_files_info("calculator", ".")
+class TestGetFileContent(unittest.TestCase):
+    def test_main(self):
+        result = get_file_content("calculator", "main.py")
         print(result + "\n")
 
-    def test_calculator_to_pkg(self):
-        result = get_files_info("calculator", "pkg")
+    def test_calculator(self):
+        result = get_file_content("calculator", "pkg/calculator.py")
         print(result + "\n")
 
-    def test_calculator_to_bin(self):
-        result = get_files_info("calculator", "/bin")
-        print(result + "\n")
-
-    def test_calculator_to_root(self):
-        result = get_files_info("calculator", "../")
+    def test_cat(self):
+        result = get_file_content("calculator", "/bin/cat")
         print(result + "\n")
 
 if __name__ == "__main__":
